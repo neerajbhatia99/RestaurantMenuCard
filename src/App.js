162dcc8d7fd820f4f61cf601a174295f8cd1b0d9
neerajbhatia99 <64474166+neerajbhatia99@users.noreply.github.com>
2024-04-13 15:11:45 +0530

    Create App.js
    added app.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import React from 'react'
+import {Restaurant} from "./component/Basics/Restaurant"
+//rafce
+const App = () => {
+  return (
+    <div>
+      return <Restaurant/>
+    </div>
+  )
+}
+
+export default App
